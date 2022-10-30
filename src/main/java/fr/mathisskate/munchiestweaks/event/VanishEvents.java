788f1f7dc49921dc5e3dd4c06e394122a3ab0db7
mathis <mathisleskateur@gmail.com>
2022-10-30 14:19:15 +0100

    Les joueurs vanish ne peuvent plus ramasser d'item

--- src/main/java/fr/mathisskate/munchiestweaks/event/VanishEvents.java
+++ src/main/java/fr/mathisskate/munchiestweaks/event/VanishEvents.java
@@ -6,23 +6,12 @@ import net.minecraft.potion.Effects;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderPlayerEvent;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.living.LivingSetAttackTargetEvent;
+import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class VanishEvents {
-    @SubscribeEvent
-    public void onVanishedPlayerHurt(LivingHurtEvent event) {
-        if (event.getEntity() != null)
-            if (event.getEntity() instanceof PlayerEntity) {
-                PlayerEntity player = (PlayerEntity) event.getEntity();
-                if (player.getActivePotionEffect(Effects.INVISIBILITY) != null)
-                    if (player.getActivePotionEffect(Effects.INVISIBILITY).getAmplifier() == 99)
-                        event.setCanceled(true);
-            }
-    }
-
     @SubscribeEvent
     public void onLivingTargetVanishedPlayer(LivingSetAttackTargetEvent event) {
         if (event.getTarget() != null)
@@ -40,7 +29,17 @@ public class VanishEvents {
 
     @OnlyIn(Dist.CLIENT)
     @SubscribeEvent(priority = EventPriority.LOW)
-    public void onPreHideVanishedPlayer(RenderPlayerEvent event) {
+    public void onHideVanishedPlayer(RenderPlayerEvent event) {
+        if (event.getPlayer() != null) {
+            PlayerEntity player = event.getPlayer();
+            if (player.getActivePotionEffect(Effects.INVISIBILITY) != null)
+                if (player.getActivePotionEffect(Effects.INVISIBILITY).getAmplifier() == 99)
+                    event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
+    public void onVanishedPlayerPickItem(EntityItemPickupEvent event) {
         if (event.getPlayer() != null) {
             PlayerEntity player = event.getPlayer();
             if (player.getActivePotionEffect(Effects.INVISIBILITY) != null)
