f84e7f211fba4225a78561d79cfa2b186d8fb258
mathis <mathisleskateur@gmail.com>
2022-11-01 12:46:38 +0100

    Changement des coordonées first spawn de INTEGER EN DOUBLE

--- src/main/java/fr/mathisskate/munchiestweaks/util/FirstSpawnConfig.java
+++ src/main/java/fr/mathisskate/munchiestweaks/util/FirstSpawnConfig.java
@@ -6,15 +6,15 @@ public class FirstSpawnConfig {
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     public static final ForgeConfigSpec SPEC;
 
-    public static final ForgeConfigSpec.ConfigValue<Integer> X;
-    public static final ForgeConfigSpec.ConfigValue<Integer> Y;
-    public static final ForgeConfigSpec.ConfigValue<Integer> Z;
+    public static final ForgeConfigSpec.ConfigValue<Double> X;
+    public static final ForgeConfigSpec.ConfigValue<Double> Y;
+    public static final ForgeConfigSpec.ConfigValue<Double> Z;
 
     static {
         BUILDER.push("Config of First Spawn");
-        X = BUILDER.comment("First Spawn X").define("X", 0);
-        Y = BUILDER.comment("First Spawn Y").define("Y", 250);
-        Z = BUILDER.comment("First Spawn Z").define("Z", 0);
+        X = BUILDER.comment("First Spawn X").define("X", 0.5);
+        Y = BUILDER.comment("First Spawn Y").define("Y", 250.0);
+        Z = BUILDER.comment("First Spawn Z").define("Z", 0.5);
         BUILDER.pop();
         SPEC = BUILDER.build();
     }
